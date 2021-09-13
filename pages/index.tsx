d3f62d40cccda5490170bd3bc4d61206c144011a
abhrajitray77 <abhrajitray77@gmail.com>
2021-09-13 20:55:07 +0530

    Updated page

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,7 +14,7 @@ const Home: NextPage = () => {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome Abhrajit Ray!
+          Welcome Abhrajit Ray! To Nextjs
         </h1>
 
         <p className={styles.description}>
